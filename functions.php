378210f21f0b75718f8683e6c92fe3dcc7df9f60
motestmo <mohammad.yousif@geomatrix.group>
2025-06-20 14:25:22 +0300

    WORKING ALSO

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! defined( 'MOBOOKING_VERSION' ) ) {
-    define( 'MOBOOKING_VERSION', '0.1.0' );
+    define( 'MOBOOKING_VERSION', '0.1.6' );
 }
 if ( ! defined( 'MOBOOKING_THEME_DIR' ) ) {
     define( 'MOBOOKING_THEME_DIR', trailingslashit( get_template_directory() ) );
